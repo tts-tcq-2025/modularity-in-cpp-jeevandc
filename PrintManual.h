cf46f7a63936695e3bad5b4cc99b00ebf9b4099d
Jeevan DC <jeevan.9571@gmail.com>
2025-08-11 23:13:13 +0530

    Create PrintManual.h

---
+++ PrintManual.h
@@ -0,0 +1,10 @@
+#ifndef PRINTCOLORCODE_H
+#define PRINTCOLORCODE_H
+
+namespace TeliComWireColorCode
+{
+	// Function to print the 25-pair color code reference table
+	void printColorCodeManual();
+}
+
+#endif
