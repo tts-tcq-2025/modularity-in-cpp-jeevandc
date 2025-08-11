7c2ba07a73f9872fb714f7c902c74713bd58d74e
Jeevan DC <jeevan.9571@gmail.com>
2025-08-11 22:43:35 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "ColorCode.h"
 
 int main() {
-    printNumberToPair(4);
-    printNumberToPair(5);
+    TeliComWireColorCode::printNumberToPair(4);
+    TeliComWireColorCode::printNumberToPair(5);
 
-    printPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE);
-    printPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE);
+    TeliComWireColorCode::printPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE);
+    TeliComWireColorCode::printPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE);
 
     return 0;
 }
